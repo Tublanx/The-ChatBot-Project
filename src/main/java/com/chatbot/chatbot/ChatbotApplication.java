2938184e77b0ff94c40d338401bf89ad1144c159
Tublanx <rlgus03453@naver.com>
2022-06-01 22:04:59 +0900

    Initial Commit

--- src/main/java/com/chatbot/chatbot/ChatbotApplication.java
+++ src/main/java/com/chatbot/chatbot/ChatbotApplication.java
@@ -1,7 +1,9 @@
 package com.chatbot.chatbot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ChatbotApplication {
 
 	public static void main(String[] args) {
