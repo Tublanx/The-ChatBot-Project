369e37aa9e6a1554001c392830e5b65552426597
Tublanx <rlgus03453@naver.com>
2022-06-01 19:39:39 +0900

    extends SpringBootServletInitializer and Override configure method

--- src/main/java/com/chatbot/chatbot/ChatbotApplication.java
+++ src/main/java/com/chatbot/chatbot/ChatbotApplication.java
@@ -2,12 +2,18 @@ package com.chatbot.chatbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ChatbotApplication {
+public class ChatbotApplication extends SpringBootServletInitializer { // deploy war
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatbotApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ChatbotApplication.class);
+	}
 }
