d94076161ca44c685405c5f891e3e7aa9e8ee4ea
Tublanx <rlgus03453@naver.com>
2022-06-01 21:26:50 +0900

    Initial Commit

--- src/main/java/com/chatbot/chatbot/ChatbotApplication.java
+++ src/main/java/com/chatbot/chatbot/ChatbotApplication.java
@@ -1,19 +1,10 @@
 package com.chatbot.chatbot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class ChatbotApplication extends SpringBootServletInitializer { // deploy war
+public class ChatbotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatbotApplication.class, args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(ChatbotApplication.class);
 	}
 }
